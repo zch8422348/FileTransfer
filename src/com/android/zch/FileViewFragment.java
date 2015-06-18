3e83e0693b64f0dcb9d4a308debbf9fe8508a028
zch8422348 <391767560@qq.com>
2015-06-18 15:10:46 +0800

    modified drag type

--- src/com/android/zch/FileViewFragment.java
+++ src/com/android/zch/FileViewFragment.java
@@ -1,8 +1,10 @@
 package com.android.zch;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore.Audio;
 import android.provider.MediaStore.Files;
@@ -10,34 +12,52 @@ import android.provider.MediaStore.Images;
 import android.provider.MediaStore.Video;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.TouchDelegate;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.android.zch.R;
+import com.android.zch.CategoryActivity.MyTouchListener;
 import com.android.zch.adapter.DragAdapter;
 import com.android.zch.base.BaseFragment;
 import com.android.zch.entities.ChannelItem;
 import com.android.zch.entities.FileInfoEntity;
 import com.android.zch.file.FileCategoryHelper.FileCategory;
+import com.android.zch.util.Utils;
 
-/**   
-* @Title: FileViewFragment.java 
-* @Package com.android.touchjet 
-* @Description: TODO
-* @author zch:qw8shop@gmail.com  
-* @date 2015-2-3 上午10:58:24 
-* @version V1.0   
-*/
-public class FileViewFragment extends BaseFragment {
+/**
+ * @Title: FileViewFragment.java
+ * @Package com.android.touchjet
+ * @Description: TODO
+ * @author zch:qw8shop@gmail.com
+ * @date 2015-2-3 上午10:58:24
+ * @version V1.0
+ */
+public class FileViewFragment extends BaseFragment implements MyTouchListener,
+		OnItemLongClickListener {
 	private View rootView;
 	private CategoryActivity activity;
 	private DragAdapter adapter;
 	private GridView gridView;
 	private LinearLayout emptyView;
+	private boolean drag = false;
+	private ImageView drag_img;
+	private int lastX, lastY, left, right, top, bottom;
+	private int position = 0;
+	private int viewW = 0;
+	private int viewH = 0;
+	private Bitmap bitmap;
+	private List<FileInfoEntity> fileInfoEntities = new ArrayList<FileInfoEntity>();
 
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
@@ -45,6 +65,7 @@ public class FileViewFragment extends BaseFragment {
 				null);
 		Log.e("ss", "*********");
 		activity = (CategoryActivity) getActivity();
+		((CategoryActivity) getActivity()).registerTouchEvent(this);
 	}
 
 	@Override
@@ -63,21 +84,22 @@ public class FileViewFragment extends BaseFragment {
 		// TODO Auto-generated method stub
 		emptyView = (LinearLayout) rootView.findViewById(R.id.empty_view);
 		gridView = (GridView) rootView.findViewById(R.id.gridview);
-		adapter = new DragAdapter(getActivity(), activity.getData(), gridView,
+		drag_img = (ImageView) rootView.findViewById(R.id.touch_img);
+		fileInfoEntities = activity.getData();
+		adapter = new DragAdapter(getActivity(), fileInfoEntities, gridView,
 				getFileCategory());
-		if(FileCategory.Apk.equals(activity.getFileCategory())){
+		if (FileCategory.Apk.equals(activity.getFileCategory())) {
 			gridView.setNumColumns(4);
 		}
 		gridView.setAdapter(adapter);
+		left = drag_img.getLeft();
+		top = drag_img.getTop();
+		right = drag_img.getRight();
+		bottom = drag_img.getBottom();
+		initEvents();
 		return rootView;
 	}
 
-	
-	
-	
-	
-	
-	
 	public void notifyAdapterData(List<FileInfoEntity> fileInfoEntities) {
 		adapter.setListDate(fileInfoEntities);
 	}
@@ -88,33 +110,75 @@ public class FileViewFragment extends BaseFragment {
 		gridView.setVisibility(View.GONE);
 	}
 
-	// public void getCategoryAdapter(FileCategory fc) {
-	// switch (fc) {
-	// case Doc:
-	// case Zip:
-	// case Apk:
-	// break;
-	// case Music:
-	// adapter = new DragAdapter(getActivity(), activity.getData(),
-	// gridView, fc);
-	// break;
-	// case Video:
-	// adapter = new DragAdapter(getActivity(), activity.getData(),
-	// gridView);
-	// break;
-	// case Picture:
-	// adapter = new DragAdapter(getActivity(), activity.getData(),
-	// gridView);
-	// break;
-	// default:
-	// break;
-	// }
-	// }
-
 	@Override
 	protected void initEvents() {
 		// TODO Auto-generated method stub
-
+		gridView.setOnItemLongClickListener(this);
 	}
 
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		((CategoryActivity) getActivity()).unregisterTouchEvent(this);
+		if (bitmap != null) {
+			bitmap.recycle();
+			bitmap = null;
+		}
+	}
+
+	@Override
+	public void onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if (drag)
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				lastX = (int) event.getRawX();
+				lastY = (int) event.getRawY();
+
+				left = (int) event.getX() - (drag_img.getWidth() / 2);
+				top = (int) event.getY() - (drag_img.getHeight());
+				right = left + drag_img.getWidth();
+				bottom = top + drag_img.getHeight();
+				drag_img.layout(left, top, right, bottom);
+				drag_img.setVisibility(0);
+				break;
+			/**
+			 * layout(l,t,r,b) l Left position, relative to parent t Top
+			 * position, relative to parent r Right position, relative to parent
+			 * b Bottom position, relative to parent
+			 * */
+			case MotionEvent.ACTION_MOVE:
+				left = (int) event.getX() - (drag_img.getWidth() / 2);
+				top = (int) event.getY() - (drag_img.getHeight());
+				right = left + drag_img.getWidth();
+				bottom = top + drag_img.getHeight();
+				drag_img.layout(left, top, right, bottom);
+				drag_img.setVisibility(View.VISIBLE);
+				drag_img.setImageBitmap(bitmap);
+				if (left < -30) {
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+				drag = false;
+				gridView.setEnabled(true);
+				drag_img.setVisibility(4);
+				break;
+			default:
+				break;
+			}
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> arg0, View view, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		drag = true;
+		viewW = view.getWidth();
+		viewH = view.getHeight();
+		bitmap = Utils.getBitmapFromFile(new File(fileInfoEntities
+				.get(position).getFileData()), viewW, viewH);
+		gridView.setEnabled(false);
+		return false;
+	}
 }
--- src/com/android/zch/util/Utils.java
+++ src/com/android/zch/util/Utils.java
@@ -1,20 +1,22 @@
 package com.android.zch.util;
 
+import java.io.File;
 import java.lang.reflect.Field;
 
 import android.content.Context;
-import android.net.wifi.WifiInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.wifi.WifiManager;
 import android.view.ViewConfiguration;
 
-/**   
-* @Title: Utils.java 
-* @Package com.android.touchjet.util 
-* @Description: TODO 
-* @author spring:qw8shop@gmail.com  
-* @date 2015-2-4 上午12:01:14 
-* @version V1.0   
-*/ 
+/**
+ * @Title: Utils.java
+ * @Package com.android.touchjet.util
+ * @Description: TODO
+ * @author spring:qw8shop@gmail.com
+ * @date 2015-2-4 上午12:01:14
+ * @version V1.0
+ */
 
 public class Utils {
 
@@ -24,13 +26,15 @@ public class Utils {
 		return (wifimanage.getConnectionInfo() == null) ? ""
 				: intToIp(wifimanage.getConnectionInfo().getIpAddress());
 	}
+
 	// 将获取的int转为真正的ip地址
 	public static String intToIp(int paramIntip) {
 		return (paramIntip & 0xFF) + "." + ((paramIntip >> 8) & 0xFF) + "."
 				+ ((paramIntip >> 16) & 0xFF) + "."
 				+ ((paramIntip >> 24) & 0xFF);
 	}
-	public static void showActionBarMenu(Context context){
+
+	public static void showActionBarMenu(Context context) {
 		try {
 			ViewConfiguration mconfig = ViewConfiguration.get(context);
 			Field menuKeyField = ViewConfiguration.class
@@ -40,6 +44,72 @@ public class Utils {
 				menuKeyField.setBoolean(mconfig, false);
 			}
 		} catch (Exception ex) {
+		}
+	}
+
+	public static Bitmap getBitmapFromFile(File dst, int width, int height) {
+		if (null != dst && dst.exists()) {
+			BitmapFactory.Options opts = null;
+			if (width > 0 && height > 0) {
+				opts = new BitmapFactory.Options(); // 设置inJustDecodeBounds为true后，decodeFile并不分配空间，此时计算原始图片的长度和宽度
+				opts.inJustDecodeBounds = true;
+				BitmapFactory.decodeFile(dst.getPath(), opts);
+				// 计算图片缩放比例
+				final int minSideLength = Math.min(width, height);
+				opts.inSampleSize = computeSampleSize(opts, minSideLength,
+						width * height); // 这里一定要将其设置回false，因为之前我们将其设置成了true
+				opts.inJustDecodeBounds = false;
+				opts.inInputShareable = true;
+				opts.inPurgeable = true;
+			}
+			try {
+				return BitmapFactory.decodeFile(dst.getPath(), opts);
+			} catch (OutOfMemoryError e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public static int computeSampleSize(BitmapFactory.Options options,
+			int minSideLength, int maxNumOfPixels) {
+		int initialSize = computeInitialSampleSize(options, minSideLength,
+				maxNumOfPixels);
+
+		int roundedSize;
+		if (initialSize <= 8) {
+			roundedSize = 1;
+			while (roundedSize < initialSize) {
+				roundedSize <<= 1;
+			}
+		} else {
+			roundedSize = (initialSize + 7) / 8 * 8;
+		}
+
+		return roundedSize;
+	}
+
+	private static int computeInitialSampleSize(BitmapFactory.Options options,
+			int minSideLength, int maxNumOfPixels) {
+		double w = options.outWidth;
+		double h = options.outHeight;
+
+		int lowerBound = (maxNumOfPixels == -1) ? 1 : (int) Math.ceil(Math
+				.sqrt(w * h / maxNumOfPixels));
+		int upperBound = (minSideLength == -1) ? 128 : (int) Math.min(
+				Math.floor(w / minSideLength), Math.floor(h / minSideLength));
+
+		if (upperBound < lowerBound) {
+			// return the larger one when there is no overlapping zone.
+			return lowerBound;
+		}
+
+		if ((maxNumOfPixels == -1) && (minSideLength == -1)) {
+			return 1;
+		} else if (minSideLength == -1) {
+			return lowerBound;
+		} else {
+			return upperBound;
 		}
 	}
 }
